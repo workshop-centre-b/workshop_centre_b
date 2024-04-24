188d09f758040e29e4ae470f757096addbc3227d
jeremy-di <delta.i.developpement@gmail.com>
2024-04-24 09:31:18 +0200

    modification formulaire plat et menu

--- src/Form/MenuType.php
+++ src/Form/MenuType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use App\Entity\Menu;
 use App\Entity\Plat;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Repository\PlatRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MenuType extends AbstractType
@@ -21,23 +22,48 @@ class MenuType extends AbstractType
             ->add('optionPlat')
             ->add('entree', EntityType::class, [
                 'class' => Plat::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
+                'query_builder' => function (PlatRepository $er) {
+                    return $er->createQueryBuilder('p')
+                        ->where('p.type = :type')
+                        ->setParameter('type', 'Entree');
+                },
             ])
             ->add('platUn', EntityType::class, [
                 'class' => Plat::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
+                'query_builder' => function (PlatRepository $er) {
+                    return $er->createQueryBuilder('p')
+                        ->where('p.type = :type')
+                        ->setParameter('type', 'Plat');
+                },
             ])
             ->add('platDeux', EntityType::class, [
                 'class' => Plat::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
+                'query_builder' => function (PlatRepository $er) {
+                    return $er->createQueryBuilder('p')
+                        ->where('p.type = :type')
+                        ->setParameter('type', 'Plat');
+                },
             ])
             ->add('platTrois', EntityType::class, [
                 'class' => Plat::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
+                'query_builder' => function (PlatRepository $er) {
+                    return $er->createQueryBuilder('p')
+                        ->where('p.type = :type')
+                        ->setParameter('type', 'Plat');
+                },
             ])
             ->add('dessert', EntityType::class, [
                 'class' => Plat::class,
-'choice_label' => 'id',
+                'choice_label' => 'id',
+                'query_builder' => function (PlatRepository $er) {
+                    return $er->createQueryBuilder('p')
+                        ->where('p.type = :type')
+                        ->setParameter('type', 'Dessert');
+                },
             ])
         ;
     }
