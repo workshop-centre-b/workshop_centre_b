cfd50494eafd961a08451c4f9cf0eb87d2302231
jeremy-di <delta.i.developpement@gmail.com>
2024-04-24 09:25:03 +0200

    crud plat relations menu

--- src/Entity/Menu.php
+++ src/Entity/Menu.php
@@ -25,6 +25,25 @@ class Menu
     #[ORM\Column(length: 255)]
     private ?string $optionPlat = null;
 
+    #[ORM\ManyToOne(inversedBy: 'menus')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Plat $entree = null;
+
+    #[ORM\ManyToOne(inversedBy: 'menus')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Plat $platUn = null;
+
+    #[ORM\ManyToOne(inversedBy: 'menus')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Plat $platDeux = null;
+
+    #[ORM\ManyToOne(inversedBy: 'menus')]
+    private ?Plat $platTrois = null;
+
+    #[ORM\ManyToOne(inversedBy: 'menus')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Plat $dessert = null;
+
     public function __construct()
     {
         $this->plats = new ArrayCollection();
@@ -67,6 +86,66 @@ class Menu
     public function setOptionPlat(string $optionPlat): static
     {
         $this->optionPlat = $optionPlat;
+
+        return $this;
+    }
+
+    public function getEntree(): ?Plat
+    {
+        return $this->entree;
+    }
+
+    public function setEntree(?Plat $entree): static
+    {
+        $this->entree = $entree;
+
+        return $this;
+    }
+
+    public function getPlatUn(): ?Plat
+    {
+        return $this->platUn;
+    }
+
+    public function setPlatUn(?Plat $platUn): static
+    {
+        $this->platUn = $platUn;
+
+        return $this;
+    }
+
+    public function getPlatDeux(): ?Plat
+    {
+        return $this->platDeux;
+    }
+
+    public function setPlatDeux(?Plat $platDeux): static
+    {
+        $this->platDeux = $platDeux;
+
+        return $this;
+    }
+
+    public function getPlatTrois(): ?Plat
+    {
+        return $this->platTrois;
+    }
+
+    public function setPlatTrois(?Plat $platTrois): static
+    {
+        $this->platTrois = $platTrois;
+
+        return $this;
+    }
+
+    public function getDessert(): ?Plat
+    {
+        return $this->dessert;
+    }
+
+    public function setDessert(?Plat $dessert): static
+    {
+        $this->dessert = $dessert;
 
         return $this;
     }
---
+++ src/Form/PlatType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Allergen;
+use App\Entity\Plat;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('composition')
+            ->add('type')
+            ->add('regime')
+            ->add('allergenes', EntityType::class, [
+                'class' => Allergen::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Plat::class,
+        ]);
+    }
+}
